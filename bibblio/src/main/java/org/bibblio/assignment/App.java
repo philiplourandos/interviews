820d8b7f9e84d048b073a37f5de920be2a07cea4
Philip Lourandos <philip.lourandos@gmail.com>
2016-08-28 11:49:46 +0200

    # Implemented App class

--- bibblio/src/main/java/org/bibblio/assignment/App.java
+++ bibblio/src/main/java/org/bibblio/assignment/App.java
@@ -1,11 +1,51 @@
 package org.bibblio.assignment;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  *
  * @author plourand
  */
 public class App {
+    private static final Logger LOG = LogManager.getLogger();
+    
+    private static final int MAX_MARS_SIZE = 50;
+
     public static void main(String[] args) {
+        String[] marsDimensions = args[0].split(" ");
+        int length = Integer.valueOf(marsDimensions[0]);
+        int breadth = Integer.valueOf(marsDimensions[1]);
+
+        if (length > MAX_MARS_SIZE || breadth > MAX_MARS_SIZE) {
+            LOG.error("The map size exceeds Mar's real estate");
+
+            return;
+        }
+
+        String[][] terrain = new String[length][breadth];
         
+        List<RoverCommand> commands = new ArrayList<>();
+        
+        for(int index = 1; index < args.length; index++) {
+            final String initialPositioning = args[index];
+            final String cmds = args[++index];
+            
+            commands.add(new RoverCommand(initialPositioning, cmds));
+        }
+        
+        for(RoverCommand currentCmd : commands) {
+            final Rover curiocity = new Rover(currentCmd.getOrientation(), terrain, currentCmd.getStartCoordintes());
+            
+            final List<String> roverInstructions = currentCmd.getInstructions();
+            
+            for(String currentInstr : roverInstructions) {
+                curiocity.executeInstruction(currentInstr);
+            }
+            
+            LOG.info("{}", curiocity);
+        }
     }
 }
