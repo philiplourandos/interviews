0a031fc2452073871ddb043d6a13aecc31b8cef7
Philip Lourandos <philip.lourandos@gmail.com>
2016-08-28 11:49:19 +0200

    # Moved constants from Rover class

--- bibblio/src/main/java/org/bibblio/assignment/Direction.java
+++ bibblio/src/main/java/org/bibblio/assignment/Direction.java
@@ -5,6 +5,11 @@ package org.bibblio.assignment;
  * @author plourand
  */
 public interface Direction {
+    public static final String S = "S";
+    public static final String E = "E";
+    public static final String W = "W";
+    public static final String N = "N";
+    
     Direction right();
     Direction left();
 
