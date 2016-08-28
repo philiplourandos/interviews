f83f485925ef6930da91c9e69a7421a3886a222e
Philip Lourandos <philip.lourandos@gmail.com>
2016-08-28 13:04:33 +0200

    # Test case for app

---
+++ bibblio/src/test/java/org/bibblio/assignment/RoverTest.java
@@ -0,0 +1,64 @@
+package org.bibblio.assignment;
+
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import org.junit.internal.runners.JUnit4ClassRunner;
+
+/**
+ *
+ * @author plourand
+ */
+@RunWith(JUnit4ClassRunner.class)
+public class RoverTest {
+    
+    private static final String[][] MARS = new String[5][3];
+    private static final RoverCommand[] SAMPLE_INPUTS = new RoverCommand[]{
+        new RoverCommand("1 1 E", "RFRFRFRF"),
+        new RoverCommand("3 2 N", "FRRFLLFFRRFLL"),
+        new RoverCommand("0 3 W", "LLFFFLFLFL")
+    };
+    
+    private static final String[] EXPECTED_OUTPUTS = {
+        "1 1 E",
+        "3 3 N LOST",
+        "2 3 S"
+    };
+
+    @Test
+    public void sampleInput() {
+        for(int index = 0; index < EXPECTED_OUTPUTS.length; index++) {
+            final RoverCommand cmd = SAMPLE_INPUTS[index];
+            List<String> instructions = cmd.getInstructions();
+
+            final Rover rover = new Rover(cmd.getOrientation(), MARS, cmd.getStartCoordintes());
+
+            System.out.println(rover);
+            
+            for(String currentInstr : instructions) {
+                System.out.println(currentInstr);
+
+                rover.executeInstruction(currentInstr);
+
+                System.out.println(rover);
+            }
+
+            System.out.println("---------------------------------------------");
+            
+            assertEquals(EXPECTED_OUTPUTS[index], rover.toString().trim());
+        }
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void testInvalidMarsSize() {
+        App.main(new String[]{"50 60", "3 2 E", "FFFFFFFF"});
+    }
+    
+    @Test(expected = RuntimeException.class)
+    public void invalidInstructions() {
+        App.main(new String[]{"50 60", "3 2 E", "dFFFLLRR"});
+    }
+}
