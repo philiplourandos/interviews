8419333937e16f40000eb8c3756e43b5d9930a2e
Philip Lourandos <philip.lourandos@gmail.com>
2016-09-04 13:19:50 +0200

    # Added findByName method

--- nox-rentals/nox-test-service/src/main/java/com/noxrentals/service/PropertyRepository.java
+++ nox-rentals/nox-test-service/src/main/java/com/noxrentals/service/PropertyRepository.java
@@ -2,13 +2,14 @@ package com.noxrentals.service;
 
 import com.noxrentals.domain.Property;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 /**
  *
  * @author plourand
  */
-@RepositoryRestResource(path = "property", collectionResourceRel = "property")
+@RepositoryRestResource(path = "props", collectionResourceRel = "property")
 public interface PropertyRepository extends PagingAndSortingRepository<Property, Long> {
-    
+    Property findByName(@Param("name") String name);
 }
