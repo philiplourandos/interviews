4c9190b32d1f74f1b0afc5b15896d3b087876fac
Philip Lourandos <philip.lourandos@gmail.com>
2016-09-04 13:20:51 +0200

    # Implemented delete method

--- nox-rentals/nox-test-service/src/test/java/com/noxrentals/NoxApplicationTests.java
+++ nox-rentals/nox-test-service/src/test/java/com/noxrentals/NoxApplicationTests.java
@@ -1,35 +1,36 @@
 package com.noxrentals;
 
 import com.noxrentals.service.PropertyRepository;
+import org.json.JSONObject;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import static org.hamcrest.Matchers.hasSize;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 @DataJpaTest
 public class NoxApplicationTests {
     
@@ -66,27 +67,34 @@ public class NoxApplicationTests {
 
     @Test
     public void successList() throws Exception {
-        mvc.perform(get("/api/property"))
+        mvc.perform(get("/api/props"))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
                 .andExpect(jsonPath("$", hasSize(LIST_EXPECTED_COUNT)));
     }
 
     @Test
-    public void add() throws Exception {
-        mvc.perform(post("/api/property").contentType(MediaType.APPLICATION_JSON).content(ADD_PROPERTY_PAYLOAD))
+    public void successAdd() throws Exception {
+        mvc.perform(post("/api/props").contentType(MediaType.APPLICATION_JSON).content(ADD_PROPERTY_PAYLOAD))
                 .andExpect(status().isCreated());
                 
         assertEquals(ADD_EXPECTED_COUNT, repo.findAll().spliterator().getExactSizeIfKnown());
     }
 
     @Test
-    public void update() throws Exception {
+    public void successUpdate() throws Exception {
         fail();
     }
 
     @Test
-    public void delete() throws Exception {
-        fail();
+    public void successDelete() throws Exception {
+        successAdd();
+
+        MvcResult result = mvc.perform(get("/api/props/search/findByName?name=Buffets")).andReturn();
+        JSONObject json = new JSONObject(result.getResponse().getContentAsString());
+
+        String id = json.getString("id");
+
+        mvc.perform(delete("/api/props/{id}", id)).andExpect(status().isOk());
     }
 }
